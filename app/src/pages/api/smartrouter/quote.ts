47373963ccfb77c344214dfccefdb4d83ef66571
leo <leean1687@gmail.com>
2025-10-14 21:53:14 +0800

    feat: Return original parameters

--- app/src/pages/api/smartrouter/quote.ts
+++ app/src/pages/api/smartrouter/quote.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: leo leean1687@gmail.com
+ * @Date: 2025-10-09 17:59:41
+ * @LastEditors: leo leean1687@gmail.com
+ * @LastEditTime: 2025-10-14 21:52:55
+ * @FilePath: /app/src/pages/api/smartrouter/quote.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { ethers } from 'ethers'
 import { AlphaRouter, SwapOptionsSwapRouter02, SwapType } from '@uniswap/smart-order-router'
 import { Percent, Token, CurrencyAmount } from '@uniswap/sdk-core'
@@ -59,5 +67,10 @@ export const getRoute = async (params: ParamsOptions) => {
     TRADE_TYPE,
     options
   );
-  return route
+  return {
+    route,
+    params:{
+      token0, token1, walletAddress, slippage, amount, tradeType
+    }
+  }
 }
