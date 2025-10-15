1150be5a6ed422a769b558695ae1e2acb78d5d49
Sonami <sonami@cyberapper.ai>
2025-10-15 10:40:06 +0800

    refactor: Remove unnecessary comments and tidy up parameter formatting in quote.ts

--- app/src/pages/api/smartrouter/quote.ts
+++ app/src/pages/api/smartrouter/quote.ts
@@ -1,11 +1,3 @@
-/*
- * @Author: leo leean1687@gmail.com
- * @Date: 2025-10-09 17:59:41
- * @LastEditors: leo leean1687@gmail.com
- * @LastEditTime: 2025-10-14 21:52:55
- * @FilePath: /app/src/pages/api/smartrouter/quote.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import { ethers } from 'ethers'
 import { AlphaRouter, SwapOptionsSwapRouter02, SwapType } from '@uniswap/smart-order-router'
 import { Percent, Token, CurrencyAmount } from '@uniswap/sdk-core'
@@ -69,7 +61,7 @@ export const getRoute = async (params: ParamsOptions) => {
   );
   return {
     route,
-    params:{
+    params: {
       token0, token1, walletAddress, slippage, amount, tradeType
     }
   }
